8d124bec38a31fd7bec106352b43f90f6a837a29
Keval Gondaliya <keval.gondaliya.km@gmail.com>
2024-08-12 20:48:18 +0530

    added delimiter changes and added validation for the negative numbers

--- src/App.js
+++ src/App.js
@@ -1,42 +1,92 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState(0);
+
   // Added function for the sum of values
   const add = (values) => {
     if (values === "") {
       return 0;
     }
-    const sumArray = values ? values.split(/,|\n/) : [];
 
+    let delimiter = /,|\n/;
+    const negativeNumbers = [];
+
+    if (values.startsWith("//")) {
+      const deliCompare = values.match(/\/\/(.)\n/);
+      if (deliCompare) {
+        delimiter = new RegExp(deliCompare[1]);
+        values = values.substring(4);
+      }
+    }
+
+    const sumArray = values.split(delimiter);
     let sum = 0;
 
     for (const number of sumArray) {
-      sum += parseInt(number);
+      const parsedNumber = parseInt(number);
+      if (isNaN(parsedNumber)) {
+        console.log(isNaN(parsedNumber));
+        throw new Error("Invalid number");
+      }
+
+      if (parsedNumber < 0) {
+        negativeNumbers.push(parsedNumber);
+      } else {
+        sum += parsedNumber;
+      }
     }
+
+    if (negativeNumbers.length > 0) {
+      throw new Error(
+        `Negative numbers not allowed: ${negativeNumbers.join(", ")}`
+      );
+    }
+
     return sum;
   };
 
   // handle input change
-  const handleChangeInput = () => {};
+  const handleChangeInput = (event) => {
+    setInput(event.target.value);
+  };
 
-  add();
+  const handleCalculate = () => {
+    try {
+      const sum = add(input);
+      setResult(sum);
+    } catch (error) {
+      setResult(error.message);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <input
+        <textarea
           className="digitInput"
           type="text"
-          value="digit1"
-          onChange={() => handleChangeInput()}
+          value={input}
+          onChange={handleChangeInput}
         />
 
-        <button className="sumButton" onClick={() => {}}>
+        <button className="sumButton" onClick={handleCalculate}>
           Sum
         </button>
 
-        <div className="sumView">
-          <p>Sum Value</p>
-        </div>
+        {result > 0 ? (
+          <div className="sumView">Result: {result}</div>
+        ) : (
+          <div>
+            {result.startsWith("Negative numbers") && (
+              <div className="error-message">
+                Negative numbers are not allowed.
+              </div>
+            )}
+          </div>
+        )}
       </header>
     </div>
   );
