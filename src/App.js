a7ec3ef66cf3cdd5556397940effe358c26bf646
Keval Gondaliya <keval.gondaliya.km@gmail.com>
2024-08-12 17:42:21 +0530

    added function for the sum of values

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import "./App.css";
 
 function App() {
+  
+  // Added function for the sum of values
+  const add = (values) => {
+    console.log({ values });
+
+    if (values === "") {
+      return 0;
+    }
+
+    const sumArray = values?.split(/,|\n/);
+
+    console.log({ sumArray });
+
+    let sum = 0;
+
+    for (const number of sumArray) {
+      sum += parseInt(number);
+    }
+
+    console.log({ sum });
+
+    return sum;
+  };
+
   const handleChangeInput = () => {};
 
+  add();
   return (
     <div className="App">
       <header className="App-header">
@@ -13,7 +38,9 @@ function App() {
           onChange={() => handleChangeInput()}
         />
 
-        <button className="sumButton">Sum</button>
+        <button className="sumButton" onClick={() => {}}>
+          Sum
+        </button>
 
         <div className="sumView">
           <p>Sum Value</p>
